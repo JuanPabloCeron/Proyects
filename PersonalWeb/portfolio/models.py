3285028ee7e2e93e360b77ceaf54e935c1c27fbb
JuanPabloCeron <juanpabloceron.p@gmail.com>
2025-05-13 21:45:48 +0200

    add url space

--- PersonalWeb/portfolio/models.py
+++ PersonalWeb/portfolio/models.py
@@ -8,9 +8,10 @@ class Project(models.Model): #Esta super clase models.Model representa una clase
     title = models.CharField(max_length=200, verbose_name="Titulo") # Con ChadField hacemos que esa variable sea de tipo char, asi nuestro ORM lo guarda en la base de datos
     description = models.TextField(verbose_name="Descripción") # Cuando los textos son mas grande usamos Texfield
     image = models.ImageField(verbose_name="Imagen",upload_to="projects") #De esta manera le indicamos que las imagenes se guarden en el directorio /media/projects
+    link = models.URLField(null=True,blank =True,verbose_name="Direccion web")#Para crear campos para links
     created = models.DateTimeField(auto_now_add=True,verbose_name="Fecha de creación") #Se ejecuta solo la primera vez que se crea la instancia
     updated = models.DateTimeField(auto_now=True,verbose_name="Fecha de edición") #Se ejecuta cada vez que se actualiza una instancia
-
+    
 #------------------------------------------------------------------------------------------------------   
     
     # Creamos una subclase con metadatos 
